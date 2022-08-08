f457e93ae63d1d6b517f54c1ec6bf307c33e7191
Vinícius Cipriano <110792258+viniciuscipriano@users.noreply.github.com>
2022-08-07 22:40:34 -0300

    primeiro commit programa para receber dados

---
+++ marialarissavinicius.1questao.py
@@ -0,0 +1,22 @@
+""""""
+#variaveis 
+
+from turtle import TNavigator
+
+
+nome=str
+sobrenome=str
+idade=0
+peso=0.0
+
+#comandos
+
+nome = str(input("informe seu nome: "))
+print(nome)
+sobrenome = str(input("informe seu sobrenome: "))
+print(sobrenome)
+idade = int(input("informe sua idade: "))
+print(idade)
+peso = float(input("informe seu peso: "))
+print(peso)
+print("olá, as suas informaçoes são:", nome, sobrenome, idade, peso)
